828e0086e44df7629b552044bc3cb57c049fcf72
Juan <venegas.a.juan37@gmail.com>
2022-08-28 21:41:15 -0400

    Initial Setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+
+//require('./db.connection')
+//app.use(express.static('public'))
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs')
+
+//controller setup
+
+app.get('/bravado', (req, res) =>{
+    res.send('Bravado')
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/bravado')
+})
+
+app.listen(4000, () => {
+  console.log(`listening on port: 4000`)
+});
