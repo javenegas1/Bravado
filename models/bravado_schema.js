60824df2fe4fdcbc737dbd460ed4a1df0607246d
Juan <venegas.a.juan37@gmail.com>
2022-08-29 12:10:09 -0400

    Additional Changes

--- models/bravado_schema.js
+++ models/bravado_schema.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
+
 const postSchema = new mongoose.Schema ({
     user:{type: String, required:true},
     post:{type: String, required:true},
     timeStamp: Number,
     image: String,
     comments: [{type: Comment}]
-
-
-
-
-
 });
+
 const Comment = {
     user: {type: String, required:true},
     text:{type: String, required:true},
     timeStamp: Number,
     likeCounter: Number
-    
-
-
 }
 
 
