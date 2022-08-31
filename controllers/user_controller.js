14800b097a411336bb508fdc5d8b53a3a3a6ea3d
Juan <venegas.a.juan37@gmail.com>
2022-08-31 10:34:34 -0400

    Create User

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -16,6 +16,12 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     try{
         //hashPassword
+        // const hashPassword = bcrypt.hash(req.body.password, 10)
+        // req.body.password = hashPassword
+        //create user with username and hashPassword
+        const createUser = await User.create(req.body)
+        console.log(createUser)
+        res.redirect('/bravado/login')
     } catch(error){
         console.log(error)
         res.send(error)
@@ -29,7 +35,12 @@ router.get('/login', (req, res) => {
 
 router.post('/login', async (req, res) => {
     try{
-        //match login information with database
+        //match login information with user database
+        const authUser = await User.findOne({username: req.body.username})
+        if(!authUser) return console.log('user not found')
+        const validPassword = await bcrypt.compare(req.body.password, authUser.password)
+        if(!validPassword) return console.log('invalid user')
+        res.redirect('/')
     } catch(error){
         console.log(error)
         res.send(error)
