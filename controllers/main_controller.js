7252bfdb263c0a27b0ad2cd7983d0bca6135494b
Juan <venegas.a.juan37@gmail.com>
2022-08-31 12:22:28 -0400

    User Auth.

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -5,6 +5,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 const Review = require('../models/bravado_schema')
+const User = require('../models/user_schema')
 
 //new submissions
 router.get('/newSubmission', (req, res) => {
@@ -45,7 +46,6 @@ router.get('/:category', async (req, res) => {
 router.get('/:category/:submissionId', async (req, res) => {
     try {
         const userSubmission = await Review.findById(req.params.submissionId)
-        // console.log(comment.user)
         console.log(userSubmission);
         //provides context for delete button as well
         const context = { userSubmission: userSubmission, id: userSubmission._id }
