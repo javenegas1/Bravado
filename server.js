faeb58a53dfa24a703a5cf1bf95df33319486a22
Juan <venegas.a.juan37@gmail.com>
2022-09-02 10:33:38 -0400

    deployment prep

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.get('/', (req, res) => {
     res.redirect('/bravado')
 })
 
-app.listen(4000, () => {
-  console.log(`listening on port: 4000`)
-});
+// app.listen(4000, () => {
+//   console.log(`listening on port: 4000`)
+// });
+
+app.listen(process.env.PORT || 4000);
